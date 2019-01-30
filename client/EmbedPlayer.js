ade1ee11adf0ddcda777d37b1475b92df3b43218
Eric Feinstein <erichfeinstein@gmail.com>
2019-01-30 16:48:37 -0500

    bug fix: device transfer now happening properly

--- client/EmbedPlayer.js
+++ client/EmbedPlayer.js
@@ -1,8 +1,21 @@
 export default {
   setTrack(songId, timestamp, deviceId) {
-    console.log('Setting Track, Id:', songId, 'timestamp:', timestamp)
+    console.log('Setting Track, Id:', songId, 'timestamp:', timestamp);
+    // fetch(`https://api.spotify.com/v1/me/player`, {
+    //   method: 'PUT',
+    //   headers: {
+    //     authorization: `Bearer ${this.state.body.access_token}`,
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     uris: [`spotify:track:${songId}`],
+    //     position_ms: Date.now() - new Date(timestamp),
+    //     play: true,
+    //     device_ids: [deviceId],
+    //   }),
+    // });
     fetch(
-      `https://api.spotify.com/v1/me/player`,
+      `https://api.spotify.com/v1/me/player/play?device_id=${this.deviceId}`,
       {
         method: 'PUT',
         headers: {
@@ -12,13 +25,11 @@ export default {
         body: JSON.stringify({
           uris: [`spotify:track:${songId}`],
           position_ms: Date.now() - new Date(timestamp),
-          play: true,
-          device_ids: [deviceId]
         }),
       }
-    )
+    );
   },
-  
+
   checkForPlayer() {
     const token = this.state.body.access_token;
     if (window.Spotify) {
@@ -33,7 +44,7 @@ export default {
       this.player.connect();
     }
   },
-  
+
   createEventHandlers() {
     this.player.on('initialization_error', e => {
       console.error('init error:', e);
@@ -62,32 +73,36 @@ export default {
       // })
       this.socket.on('song-info', songInfo => {
         // console.log('socket song info running')
-        this.setTrack(songInfo.songId, songInfo.timestamp, this.state.device_id)
+        this.setTrack(
+          songInfo.songId,
+          songInfo.timestamp,
+          this.state.device_id
+        );
         this.setState({
-          currentSongId: songInfo.songId
-        })
-      })
-      this.socket.emit('room', 1)
+          currentSongId: songInfo.songId,
+        });
+      });
+      this.socket.emit('room', 1);
       console.log('deviceId:', device_id);
       this.deviceId = device_id;
       console.log('Let the music play on!');
       // this.setState({ loggedIn: true, deviceId: device_id })
       // this.setTrack(device_id);
-    })
+    });
   },
-  
+
   async transferPlaybackHere() {
-    // await fetch('https://api.spotify.com/v1/me/player', {
-    //   method: 'PUT',
-    //   headers: {
-    //     authorization: `Bearer ${this.state.token}`,
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: JSON.stringify({
-    //     device_ids: [this.deviceId],
-    //     play: true,
-    //   }),
-    // });
+    await fetch('https://api.spotify.com/v1/me/player', {
+      method: 'PUT',
+      headers: {
+        authorization: `Bearer ${this.state.token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        device_ids: [this.deviceId],
+        play: true,
+      }),
+    });
 
     //Play a track on the SoundSpace player
     fetch(
@@ -102,6 +117,6 @@ export default {
         //   uris: ['spotify:track:5oD2Z1OOx1Tmcu2mc9sLY2'],
         // }),
       }
-    )
-  }
-}
+    );
+  },
+};
