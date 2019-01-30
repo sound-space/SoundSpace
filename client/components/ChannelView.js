5394d00cfb86e6b2286ade1a87b8944470b5e149
Justin Taylor Goldberg <JustinTaylorG@Gmail.com>
2019-01-30 11:51:33 -0500

    Feature: Voting. Added component method to make PUT request to API voting route, set to onClick prop on buttons.

--- client/components/ChannelView.js
+++ client/components/ChannelView.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class ChannelView extends Component {
+  constructor() {
+    super()
+    this.state = {
+      voted: false
+    }
+  }
+  
+  vote = async (userVote) => {
+    if(this.state.voted) return
+    try {
+      await axios.put(`api/channels/${this.props.match.params.channelId}/votes`, {vote: userVote})
+      this.setState({
+        voted: true
+      })
+    }
+    catch(err) {
+      console.log(err)
+    }
+  }
+  
   render() {
     return (
       <div className='channel-view-container'>
         <h1>This is the Channel View</h1>
         
-        <button>
+        <button onClick={() => this.vote(1)}>
           Upvote!
         </button>
         
-        <button>
+        <button onClick={() => this.vote(-1)}>
           Downvote!
         </button>
       </div>
