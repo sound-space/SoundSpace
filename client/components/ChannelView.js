ad946511bdee2ad0c862a3471e6bb9a160b1a471
Justin Taylor Goldberg <JustinTaylorG@Gmail.com>
2019-01-30 11:24:00 -0500

    Component/View: ChannelView. Scaffolded new React component for displaying the view from inside a channel.

---
+++ client/components/ChannelView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ChannelView extends Component {
+  render() {
+    return (
+      <div className='channel-view-container'>
+        <h1>This is the Channel View</h1>
+      </div>
+    )
+  }
+}
