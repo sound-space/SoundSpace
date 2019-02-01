7ba6bf97b12c2b3392739505223f31e5eedf1a97
Eric Feinstein <erichfeinstein@gmail.com>
2019-02-01 12:30:21 -0500

    new feature: search in channel form (work in progress)

--- client/SpotifySearch.js
+++ client/SpotifySearch.js
@@ -1,7 +1,7 @@
-export async function search(q) {
-  q = encodeURIComponent(q);
+export async function search(query) {
+  query = encodeURIComponent(query);
   const result = await fetch(
-    `https://api.spotify.com/v1/search?q=${q}&type=track`,
+    `https://api.spotify.com/v1/search?q=${query}&type=track`,
     {
       method: 'GET',
       headers: {
--- client/components/ChannelForm.js
+++ client/components/ChannelForm.js
@@ -1,98 +1,112 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { postChannels } from '../store/channels'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { postChannels } from '../store/channels';
+import { search } from '../SpotifySearch';
 
 class ChannelForm extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.state = {
       name: '',
       imageURL: '',
       description: '',
-      songSeeds: []
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+      searchQuery: '',
+      searchResults: [],
+      songSeeds: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleChange (evt) {
+  handleChange(evt) {
     this.setState({
-      [evt.target.name]: evt.target.value
-    })
+      [evt.target.name]: evt.target.value,
+    });
   }
 
-  handleSubmit (evt) {
-    evt.preventDefault()
-    this.props.createChannels(this.state)
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.props.createChannels(this.state);
   }
 
-  render () {
-    const { name, imageURL, description } = this.state
+  async handleSearch(query) {
+    const searchResults = await search(query);
+    this.setState({
+      searchResults,
+    });
+  }
+
+  render() {
+    const { name, imageURL, description } = this.state;
 
     return (
-      <div id='modal-example' uk-modal='true'>
-        <div id='channelFormId' uk-modal='true'>
-          <div className='uk-modal-dialog uk-border-rounded'>
+      <div id="modal-example" uk-modal="true">
+        <div id="channelFormId" uk-modal="true">
+          <div className="uk-modal-dialog uk-border-rounded">
             <form onSubmit={this.handleSubmit}>
-              <fieldset className='uk-fieldset'>
-                <h2 style={{ margin: '25px' }} className='uk-modal-title'>
+              <fieldset className="uk-fieldset">
+                <h2 style={{ margin: '25px' }} className="uk-modal-title">
                   New Channel
                 </h2>
                 <div style={{ margin: '0 25px' }}>
-                  <div className='uk-margin'>
+                  <div className="uk-margin">
                     <input
                       onChange={this.handleChange}
-                      name='name'
-                      className='uk-input'
+                      name="name"
+                      className="uk-input"
                       value={name}
-                      type='text'
-                      placeholder='Name'
+                      type="text"
+                      placeholder="Name"
                       required
                     />
                   </div>
 
-                  <div className='uk-margin'>
+                  <div className="uk-margin">
                     <input
                       onChange={this.handleChange}
-                      name='imageURL'
+                      name="imageURL"
                       value={imageURL}
-                      className='uk-input'
-                      type='text'
-                      placeholder='Image URL'
+                      className="uk-input"
+                      type="text"
+                      placeholder="Image URL"
                     />
                   </div>
 
-                  <div className='uk-margin'>
+                  <div className="uk-margin">
                     <input
-                      onChange={this.handleChange}
-                      // search='search'
-                      className='uk-input'
-                      // value={search}
-                      type='text'
-                      placeholder='Search Songs'
+                      onChange={evt => this.handleSearch(evt.target.value)}
+                      className="uk-input"
+                      type="text"
+                      placeholder="Search Songs"
                       required
                     />
                   </div>
 
-                  <div className='uk-margin'>
+                  <div>
+                    {this.state.searchResults.map(track => {
+                      return <div>{track}</div>;
+                    })}
+                  </div>
+
+                  <div className="uk-margin">
                     <textarea
                       onChange={this.handleChange}
-                      name='description'
+                      name="description"
                       value={description}
-                      className='uk-textarea'
-                      rows='5'
-                      placeholder='Description'
+                      className="uk-textarea"
+                      rows="5"
+                      placeholder="Description"
                       required
                     />
                   </div>
                 </div>
-                <p className='uk-text-right' style={{ margin: '25px' }}>
+                <p className="uk-text-right" style={{ margin: '25px' }}>
                   <button
-                    className='uk-button uk-button-default uk-modal-close'
-                    type='button'
+                    className="uk-button uk-button-default uk-modal-close"
+                    type="button"
                   >
                     Cancel
                   </button>
-                  <button className='uk-button uk-button-danger' type='submit'>
+                  <button className="uk-button uk-button-danger" type="submit">
                     Submit
                   </button>
                 </p>
@@ -101,15 +115,15 @@ class ChannelForm extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 const mapDispatchToProps = dispatch => ({
-  createChannels: body => dispatch(postChannels(body))
-})
+  createChannels: body => dispatch(postChannels(body)),
+});
 
 export default connect(
   null,
   mapDispatchToProps
-)(ChannelForm)
+)(ChannelForm);
