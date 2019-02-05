adc8c03e168a30debe0b7c16bcc9f9d4557b9cd2
Mike Wong <wongmdesign@gmail.com>
2019-02-04 23:34:30 -0500

    tweaked nav text

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,32 +1,63 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from 'react'
+import { Link, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
 import { logUserOut } from '../store/user'
 
 const NavBar = props => {
-  console.log("HISTORY: ", props)
+  console.log('HISTORY: ', props)
   return (
     <nav
       uk-navbar='true'
       style={{ zIndex: '50', top: '0', position: 'fixed', width: '100%' }}
     >
       <div>
-          <img style={{ width: '100px' }} src='./assets/ss_logo.png' alt=""/>
+        <img style={{ width: '100px' }} src='./assets/ss_logo.png' alt='' />
       </div>
-      
+
       {props.user.id && (
-        <div className="uk-navbar-right" width="500">
-          <button onClick={() => props.props.history.push('/channels')} className="nav-button uk-button uk-margin-right" >LEAVE</button>
-            <button className="nav-button uk-button uk-margin-right">CHANNELS</button>
-            <div uk-dropdown="mode: hover">
-              <ul className="uk-nav uk-dropdown-nav">
-                {props.channels && props.channels.map(channel => (
-                <li key={channel.id}>
-                  <a href={`/#/channels/${channel.id}`}>{channel.name}</a>
-                  </li>))}
-              </ul>
-            </div>
-          <button className="nav-button uk-button uk-margin-right" onClick={props.logout}>LOGOUT</button>
+        <div className='uk-navbar-right' width='500'>
+          <a
+            style={{
+              fontSize: '24px',
+              fontFamily: 'Tajawal',
+              fontWeight: 'bold'
+            }}
+            onClick={() => props.props.history.push('/channels')}
+            className='nav-button uk-button uk-margin-right'
+          >
+            LEAVE
+          </a>
+          <a
+            style={{
+              fontSize: '24px',
+              fontFamily: 'Tajawal',
+              fontWeight: 'bold'
+            }}
+            className='nav-button uk-button uk-margin-right'
+          >
+            CHANNELS
+          </a>
+          <div uk-dropdown='mode: hover'>
+            <ul className='uk-nav uk-dropdown-nav'>
+              {props.channels &&
+                props.channels.map(channel => (
+                  <li key={channel.id}>
+                    <a href={`/#/channels/${channel.id}`}>{channel.name}</a>
+                  </li>
+                ))}
+            </ul>
+          </div>
+          <a
+            style={{
+              fontSize: '24px',
+              fontFamily: 'Tajawal',
+              fontWeight: 'bold'
+            }}
+            className='nav-button uk-button uk-margin-right'
+            onClick={props.logout}
+          >
+            LOGOUT
+          </a>
         </div>
       )}
     </nav>
@@ -34,15 +65,18 @@ const NavBar = props => {
 }
 
 const mapStateToProps = state => {
-  return { user: state.userObj };
+  return { user: state.userObj }
 }
 
 const mapDispatch = dispatch => {
   return {
-    logout() {
+    logout () {
       dispatch(logUserOut())
     }
   }
 }
 
-export default connect(mapStateToProps, mapDispatch)(NavBar);
+export default connect(
+  mapStateToProps,
+  mapDispatch
+)(NavBar)
