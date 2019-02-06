f3d5b55f6b2adf09b2891df7f9ecccb60244ca04
Eric Feinstein <erichfeinstein@gmail.com>
2019-02-06 12:23:43 -0500

    bug fix: channels in list of all channels are always squares

--- client/components/ChannelCard.js
+++ client/components/ChannelCard.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import { Link, withRouter } from 'react-router-dom'
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 const ChannelCard = props => (
-  <div className='uk-text-center'>
+  <div className="uk-text-center">
     <Link to={`/channels/${props.channel.id}`}>
-      <div className='uk-inline-clip uk-transition-toggle' tabIndex='0'>
+      <div className="uk-inline-clip uk-transition-toggle" tabIndex="0">
         <img
-          style={{ minWidth: '200px', minHeight: '200px', objectFit: 'cover' }}
+          style={{
+            margin: '2em',
+            width: '300px',
+            height: '300px',
+            objectFit: 'cover',
+          }}
           src={props.channel.imageURL}
         />
-        <div className='uk-transition-fade uk-position-cover uk-position-small uk-overlay uk-overlay-default uk-flex uk-flex-center uk-flex-middle'>
+        <div className="uk-transition-fade uk-position-cover uk-position-small uk-overlay uk-overlay-default uk-flex uk-flex-center uk-flex-middle">
           <p
             style={{ fontFamily: 'Tajawal', fontSize: '14px' }}
-            className='uk-h6 uk-margin-remove'
+            className="uk-h6 uk-margin-remove"
           >
             {props.channel.description}
           </p>
@@ -20,10 +25,10 @@ const ChannelCard = props => (
       </div>
     </Link>
 
-    <p style={{ fontSize: '22px' }} className='uk-margin-small-top'>
+    <p style={{ fontSize: '22px' }} className="uk-margin-small-top">
       {props.channel.name}
     </p>
   </div>
-)
+);
 
-export default withRouter(ChannelCard)
+export default withRouter(ChannelCard);
