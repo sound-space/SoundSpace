61129045833eb4d5a6963072fe4e1abff0e7c033
Eric Feinstein <erichfeinstein@gmail.com>
2019-02-06 15:41:00 -0500

    Feature: all API routes protected by a login check

--- server/api/songs.js
+++ server/api/songs.js
@@ -4,16 +4,25 @@ module.exports = router;
 
 //Post multiple songs
 router.post('/', async (req, res, next) => {
-  const songIds = req.body.songIds;
-  const channelId = Number(req.body.channelId);
-  const isSuggestion = req.body.isSuggestion;
-  const songObjs = songIds.map((songId, i) => {
-    return {
-      songId,
-      isLast: i === songIds.length - 1 && !isSuggestion,
-      channelId,
-    };
-  });
-  Song.bulkCreate(songObjs);
-  res.send('Songs created successfully');
+  try {
+    if (req.user) {
+      const songIds = req.body.songIds;
+      const channelId = Number(req.body.channelId);
+      const isSuggestion = req.body.isSuggestion;
+      const songObjs = songIds.map((songId, i) => {
+        return {
+          songId,
+          isLast: i === songIds.length - 1 && !isSuggestion,
+          channelId,
+        };
+      });
+      Song.bulkCreate(songObjs);
+      res.send('Songs created successfully');
+    } else {
+      res.send('You must be logged in to add songs to a channel');
+    }
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
