20694b567ef60dd7f47d8435995080cce7cd0b58
Justin Taylor Goldberg <JustinTaylorG@Gmail.com>
2019-02-05 21:51:31 -0500

    Changed IP to always be heroku url. Testing deploy.

--- client/components/ChannelView.js
+++ client/components/ChannelView.js
@@ -7,7 +7,7 @@ import '../styles/ChannelViewStyles.css';
 import ChannelSideBar from './ChannelSideBar';
 import { search } from '../SpotifySearch';
 import Player from './Player';
-const IP = process.env.SPOTIFY_CLIENT_ID ? 'http://soundspace-fsa.herokuapp.com' : 'http://localhost:8080'
+const IP = 'https://soundspace-fsa.herokuapp.com' 
 
 class ChannelView extends Component {
   constructor(props) {
